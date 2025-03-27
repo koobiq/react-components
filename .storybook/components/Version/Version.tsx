efe96dc1f0050c6e23970607b8c487fbbcf55473
Kamil Emeleev <emeleev.kamil@gmail.com>
2025-03-27 12:30:22 +0300

    docs: add link to the Changelog page from the version badge

--- .storybook/components/Version/Version.tsx
+++ .storybook/components/Version/Version.tsx
@@ -1,5 +1,19 @@
 import { Badge } from '@koobiq/react-components';
+import LinkTo from '@storybook/addon-links/react';
 
 import { version } from '../../../package.json';
 
-export const Version = () => <Badge className="sb-unstyled" label={version} />;
+export const Version = () => {
+  const kind = 'Changelog';
+  const name = 'Docs';
+
+  return (
+    <Badge
+      className="sb-unstyled"
+      as={LinkTo}
+      kind={kind}
+      label={version}
+      name={name}
+    />
+  );
+};
