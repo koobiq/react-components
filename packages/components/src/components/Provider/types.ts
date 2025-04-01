26c37ca3a44a5ae518dd20800b14220bbf74e151
Kamil Emeleev <emeleev.kamil@gmail.com>
2025-04-01 14:42:15 +0300

    feat(components/Provider): add `breakpointsFallback` to support SSR fallback for breakpoints

--- packages/components/src/components/Provider/types.ts
+++ packages/components/src/components/Provider/types.ts
@@ -13,7 +13,12 @@ export type Breakpoints = {
 };
 
 export type ProviderProps = {
+  /** The content of the component. */
   children?: ReactNode;
+  /** Responsive breakpoints for your application. */
   breakpoints?: Breakpoints;
+  /** SSR-fallback for responsive breakpoints for your application. */
+  breakpointsFallback?: boolean[];
+  /** The locale for your application as a [BCP 47](https://www.ietf.org/rfc/bcp/bcp47.txt) language code. Defaults to the browser/OS language setting. */
   locale?: I18nProviderProps['locale'];
 };
