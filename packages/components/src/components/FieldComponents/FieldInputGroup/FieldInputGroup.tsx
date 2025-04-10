a3569a58d6e2267f27bdbceda6adeca0805bd36d
Kamil Emeleev <emeleev.kamil@gmail.com>
2025-04-10 13:26:23 +0300

    feat(components/FieldInputGroup): add `start` and `end` support to slotProps

--- packages/components/src/components/FieldComponents/FieldInputGroup/FieldInputGroup.tsx
+++ packages/components/src/components/FieldComponents/FieldInputGroup/FieldInputGroup.tsx
@@ -7,7 +7,7 @@ import {
 } from '@koobiq/react-core';
 import { Group, useInputContext } from '@koobiq/react-primitives';
 
-import { FieldAddon } from '../FieldAddon';
+import { FieldAddon, type FieldAddonProps } from '../FieldAddon';
 
 import s from './FieldInputGroup.module.css';
 import { FieldInputGroupContext } from './FieldInputGroupContext';
@@ -20,6 +20,11 @@ export type FieldInputGroupProps = ExtendableComponentPropsWithRef<
     disabled?: boolean;
     className?: string;
     error?: boolean;
+    /** The props used for each slot inside. */
+    slotProps?: {
+      start?: FieldAddonProps;
+      end?: FieldAddonProps;
+    };
   },
   'div'
 >;
@@ -27,39 +32,43 @@ export type FieldInputGroupProps = ExtendableComponentPropsWithRef<
 export const FieldInputGroup = forwardRef<
   ComponentRef<'div'>,
   FieldInputGroupProps
->(({ children, className, startAddon, endAddon, error, ...other }, ref) => {
-  const { value } = useInputContext();
-  const hasStartAddon = !!startAddon;
-  const hasEndAddon = !!endAddon;
-  const hasValue = isNotNil(value);
+>(
+  (
+    { children, className, startAddon, endAddon, error, slotProps, ...other },
+    ref
+  ) => {
+    const { value } = useInputContext();
+    const hasStartAddon = !!startAddon;
+    const hasEndAddon = !!endAddon;
+    const hasValue = isNotNil(value);
 
-  return (
-    <Group
-      className={clsx(
-        s.base,
-        hasStartAddon && s.hasStartAddon,
-        hasEndAddon && s.hasEndAddon,
-
-        className
-      )}
-      {...other}
-      ref={ref}
-    >
-      {({ hovered, focusWithin, disabled }) => (
-        <FieldInputGroupContext.Provider
-          value={{ disabled, hovered, hasValue, focusWithin }}
-        >
-          <FieldAddon placement="start" error={error}>
-            {startAddon}
-          </FieldAddon>
-          {children}
-          <FieldAddon placement="end" error={error}>
-            {endAddon}
-          </FieldAddon>
-        </FieldInputGroupContext.Provider>
-      )}
-    </Group>
-  );
-});
+    return (
+      <Group
+        className={clsx(
+          s.base,
+          hasStartAddon && s.hasStartAddon,
+          hasEndAddon && s.hasEndAddon,
+          className
+        )}
+        {...other}
+        ref={ref}
+      >
+        {({ hovered, focusWithin, disabled }) => (
+          <FieldInputGroupContext.Provider
+            value={{ disabled, hovered, hasValue, focusWithin }}
+          >
+            <FieldAddon placement="start" error={error} {...slotProps?.start}>
+              {startAddon}
+            </FieldAddon>
+            {children}
+            <FieldAddon placement="end" error={error} {...slotProps?.end}>
+              {endAddon}
+            </FieldAddon>
+          </FieldInputGroupContext.Provider>
+        )}
+      </Group>
+    );
+  }
+);
 
 FieldInputGroup.displayName = 'FieldInputGroup';
