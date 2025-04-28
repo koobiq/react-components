2993786559f9144e274fcf572037bab6ccc44c55
Kamil Emeleev <emeleev.kamil@gmail.com>
2025-04-28 18:46:30 +0300

    feat(components/Popover): import components via `Popover.Header`, `Popover.Body`, `Popover.Footer`

--- packages/components/src/components/Popover/index.ts
+++ packages/components/src/components/Popover/index.ts
@@ -1,9 +1,20 @@
+import { DialogHeader, DialogFooter, DialogContent } from '../Dialog';
+
 export * from './Popover';
 export * from './types';
-export { DialogHeader, DialogFooter, DialogContent } from '../Dialog';
 
-export {
-  DialogHeader as PopoverHeader,
-  DialogContent as PopoverContent,
-  DialogFooter as PopoverFooter,
-} from '../Dialog';
+/**
+ * @deprecated
+ * This component has been deprecated, please use `Popover.Header` instead.
+ */
+export const PopoverHeader = DialogHeader;
+/**
+ * @deprecated
+ * This component has been deprecated, please use `Popover.Body` instead.
+ */
+export const PopoverContent = DialogContent;
+/**
+ * @deprecated
+ * This component has been deprecated, please use `Popover.Footer` instead.
+ */
+export const PopoverFooter = DialogFooter;
