9d6e187ba02c0ee91dc8385529e856237d275e3f
Kamil Emeleev <emeleev.kamil@gmail.com>
2025-05-15 10:28:25 +0300

    test: add basic tests

---
+++ packages/components/src/components/Menu/Menu.test.tsx
@@ -0,0 +1,46 @@
+import { createRef } from 'react';
+
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
+
+import { Menu } from './Menu';
+
+describe('Menu', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  const onOpenChange = vi.fn();
+
+  const baseProps = {
+    'data-testid': 'root',
+    onOpenChange,
+  };
+
+  const getRoot = () => screen.getByTestId('root');
+
+  it('should forward a ref', () => {
+    const ref = createRef<HTMLDivElement>();
+
+    render(
+      <Menu {...baseProps} ref={ref} open>
+        <Menu.Item>item</Menu.Item>
+      </Menu>
+    );
+
+    expect(ref.current).toBe(getRoot());
+  });
+
+  it('should merge a custom class name with the default ones', () => {
+    const className = 'foo';
+
+    render(
+      <Menu {...baseProps} className={className} open>
+        <Menu.Item>item</Menu.Item>
+      </Menu>
+    );
+
+    const root = getRoot();
+    expect(root?.className).toContain(className);
+  });
+});
