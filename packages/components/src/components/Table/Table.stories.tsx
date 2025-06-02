c4c0fbd1323ff7d98438c4641de73995a42398e9
Kamil Emeleev <emeleev.kamil@gmail.com>
2025-06-02 17:22:13 +0300

    chore: add the Links story

--- packages/components/src/components/Table/Table.stories.tsx
+++ packages/components/src/components/Table/Table.stories.tsx
@@ -6,6 +6,13 @@ import type { TableProps } from './types';
 const meta = {
   title: 'Components/Table',
   component: Table,
+  subcomponents: {
+    'Table.Header': Table.Header,
+    'Table.Body': Table.Body,
+    'Table.Column': Table.Column,
+    'Table.Row': Table.Row,
+    'Table.Cell': Table.Cell,
+  },
   parameters: {
     layout: 'centered',
   },
@@ -46,6 +53,35 @@ export const Base: Story = {
           <Table.Cell>log.txt</Table.Cell>
           <Table.Cell>Text Document</Table.Cell>
           <Table.Cell>01/18/2019</Table.Cell>
+        </Table.Row>
+      </Table.Body>
+    </Table>
+  ),
+};
+
+export const Links: Story = {
+  render: (args: TableProps<object>) => (
+    <Table aria-label="Bookmarks" {...args}>
+      <Table.Header>
+        <Table.Column>Name</Table.Column>
+        <Table.Column>URL</Table.Column>
+        <Table.Column>Date added</Table.Column>
+      </Table.Header>
+      <Table.Body>
+        <Table.Row href="https://adobe.com/" target="_blank">
+          <Table.Cell>Adobe</Table.Cell>
+          <Table.Cell>https://adobe.com/</Table.Cell>
+          <Table.Cell>January 28, 2023</Table.Cell>
+        </Table.Row>
+        <Table.Row href="https://google.com/" target="_blank">
+          <Table.Cell>Google</Table.Cell>
+          <Table.Cell>https://google.com/</Table.Cell>
+          <Table.Cell>April 5, 2023</Table.Cell>
+        </Table.Row>
+        <Table.Row href="https://nytimes.com/" target="_blank">
+          <Table.Cell>New York Times</Table.Cell>
+          <Table.Cell>https://nytimes.com/</Table.Cell>
+          <Table.Cell>July 12, 2023</Table.Cell>
         </Table.Row>
       </Table.Body>
     </Table>
