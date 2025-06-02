d7a4a8190c161458a9ea56152af58e864d284f12
Kamil Emeleev <emeleev.kamil@gmail.com>
2025-06-02 18:05:01 +0300

    chore: add the `divider` prop

--- packages/components/src/components/Table/types.ts
+++ packages/components/src/components/Table/types.ts
@@ -1,10 +1,23 @@
-import type { ComponentRef, CSSProperties, ReactElement } from 'react';
+import type { ComponentRef, CSSProperties, ReactElement, Ref } from 'react';
 
 import type { TableStateProps } from '@koobiq/react-primitives';
+
+export const tablePropDivider = ['none', 'row', 'column'] as const;
+
+export type TablePropDivider = (typeof tablePropDivider)[number];
 
 export type TableProps<T> = {
   /** Inline styles. */
   style?: CSSProperties;
+  /** Additional CSS-classes. */
+  className?: string;
+  /**
+   * Type of separators in the table.
+   * @default none
+   */
+  divider?: TablePropDivider;
+  /** Ref to the control */
+  ref?: Ref<HTMLTableElement>;
 } & TableStateProps<T>;
 
 export type TableComponentProp = <T extends object>(
