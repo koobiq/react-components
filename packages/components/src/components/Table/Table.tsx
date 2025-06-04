d31afe15d676969d4cfbbd37c9b478a34e2a2ff9
Kamil Emeleev <emeleev.kamil@gmail.com>
2025-06-04 17:36:41 +0300

    chore: disable endpoints for unavailable functionality

--- packages/components/src/components/Table/Table.tsx
+++ packages/components/src/components/Table/Table.tsx
@@ -26,21 +26,18 @@ function TableRender<T extends object>(
     stickyHeader = false,
     fullWidth = false,
     divider = 'none',
-    selectionBehavior,
-    selectionMode,
     className,
     style,
+    ...other
   } = props;
 
   const state = useTableState({
     ...props,
-    showSelectionCheckboxes:
-      selectionMode === 'multiple' && selectionBehavior !== 'replace',
   });
 
   const domRef = useDOMRef<HTMLTableElement>(ref);
   const { collection } = state;
-  const { gridProps } = useTable({ 'aria-label': '' }, state, domRef);
+  const { gridProps } = useTable(props, state, domRef);
 
   const tableProps = mergeProps(
     {
@@ -51,6 +48,7 @@ function TableRender<T extends object>(
       style,
       ref: domRef,
     },
+    other,
     gridProps
   );
 
@@ -59,36 +57,22 @@ function TableRender<T extends object>(
       <TableRowGroup type="thead">
         {collection.headerRows.map((headerRow) => (
           <TableHeaderRow key={headerRow.key} item={headerRow} state={state}>
-            {[...headerRow.childNodes].map((column) =>
-              column.props.isSelectionCell ? (
-                <div key={column.key}>SelectionCell</div>
-              ) : (
-                // <TableSelectAllCell
-                //   key={column.key}
-                //   column={column}
-                //   state={state}
-                // />
-                <TableColumnHeader
-                  key={column.key}
-                  column={column}
-                  state={state}
-                />
-              )
-            )}
+            {[...headerRow.childNodes].map((column) => (
+              <TableColumnHeader
+                key={column.key}
+                column={column}
+                state={state}
+              />
+            ))}
           </TableHeaderRow>
         ))}
       </TableRowGroup>
       <TableRowGroup type="tbody">
         {[...collection.body.childNodes].map((row) => (
           <TableRow key={row.key} item={row} state={state}>
-            {[...row.childNodes].map((cell) =>
-              cell.props.isSelectionCell ? (
-                <div key={cell.key}>Checkbox</div>
-              ) : (
-                // <TableCheckboxCell key={cell.key} cell={cell} state={state} />
-                <TableCell key={cell.key} cell={cell} state={state} />
-              )
-            )}
+            {[...row.childNodes].map((cell) => (
+              <TableCell key={cell.key} cell={cell} state={state} />
+            ))}
           </TableRow>
         ))}
       </TableRowGroup>
--- packages/components/src/components/Table/types.ts
+++ packages/components/src/components/Table/types.ts
@@ -1,34 +1,45 @@
 import type { ComponentRef, CSSProperties, ReactElement, Ref } from 'react';
 
+import type { ExtendableComponentPropsWithRef } from '@koobiq/react-core';
 import type { TableStateProps } from '@koobiq/react-primitives';
 
 export const tablePropDivider = ['none', 'row'] as const;
 
 export type TablePropDivider = (typeof tablePropDivider)[number];
 
-export type TableProps<T> = {
-  /** Inline styles. */
-  style?: CSSProperties;
-  /** Additional CSS-classes. */
-  className?: string;
-  /**
-   * Type of separators in the table.
-   * @default none
-   */
-  divider?: TablePropDivider;
-  /**
-   * If `true`, the table will take up the full width of its container.
-   * @default false
-   * */
-  fullWidth?: boolean;
-  /**
-   * Flag indicating a fixed table header.
-   * @default false
-   * */
-  stickyHeader?: boolean;
-  /** Ref to the control. */
-  ref?: Ref<HTMLTableElement>;
-} & TableStateProps<T>;
+export type TablePropChildren<T> = TableStateProps<T>['children'];
+
+export type TableProps<T> = ExtendableComponentPropsWithRef<
+  {
+    /** Inline styles. */
+    style?: CSSProperties;
+    /** Additional CSS-classes. */
+    className?: string;
+    /**
+     * Type of separators in the table.
+     * @default none
+     */
+    divider?: TablePropDivider;
+    /**
+     * If `true`, the table will take up the full width of its container.
+     * @default false
+     * */
+    fullWidth?: boolean;
+    /**
+     * Flag indicating a fixed table header.
+     * @default false
+     * */
+    stickyHeader?: boolean;
+    /**
+     * The elements that make up the table.
+     * Includes the Table.Header, Table.Body, Table.Column, and Table.Row.
+     * */
+    children?: TablePropChildren<T>;
+    /** Ref to the control. */
+    ref?: Ref<HTMLTableElement>;
+  },
+  'table'
+>;
 
 export type TableComponentProp = <T extends object>(
   props: TableProps<T>
