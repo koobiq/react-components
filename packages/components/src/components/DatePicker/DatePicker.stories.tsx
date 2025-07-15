00a1b7255daf0613018b5f26241d8c217611a368
Kamil Emeleev <emeleev.kamil@gmail.com>
2025-07-15 15:26:57 +0300

    docs(DatePicker): add Uncontrolled and Controlled stories

--- packages/components/src/components/DatePicker/DatePicker.stories.tsx
+++ packages/components/src/components/DatePicker/DatePicker.stories.tsx
@@ -1,4 +1,9 @@
+import { useState } from 'react';
+
+import { getLocalTimeZone, parseDate } from '@internationalized/date';
 import type { Meta, StoryObj } from '@storybook/react';
+
+import { FlexBox, Typography, useDateFormatter } from '../../index';
 
 import { DatePicker } from './DatePicker';
 
@@ -16,4 +21,36 @@ type Story = StoryObj<typeof meta>;
 
 export const Base: Story = {
   render: () => <DatePicker label="Birth day" />,
+};
+
+export const Uncontrolled: Story = {
+  render: function Render() {
+    return (
+      <DatePicker
+        label="Date (uncontrolled)"
+        defaultValue={parseDate('2025-02-03')}
+      />
+    );
+  },
+};
+
+export const Controlled: Story = {
+  render: function Render() {
+    const [value, setValue] = useState(parseDate('2025-02-03'));
+    const formatter = useDateFormatter({ dateStyle: 'full' });
+
+    return (
+      <FlexBox gap="m" direction="column" alignItems="center">
+        <DatePicker
+          label="Date (controlled)"
+          value={value}
+          onChange={(newValue) => setValue(newValue!)}
+        />
+        <Typography>
+          Selected date:{' '}
+          {value ? formatter.format(value.toDate(getLocalTimeZone())) : '--'}
+        </Typography>
+      </FlexBox>
+    );
+  },
 };
