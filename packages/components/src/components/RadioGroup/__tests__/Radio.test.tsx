1a227933bd7b788c30637e6a0f2fb97b0f15426f
Kamil Emeleev <emeleev.kamil@gmail.com>
2025-07-21 14:05:19 +0300

    test(RadioGroup): add basic tests for business logic

---
+++ packages/components/src/components/RadioGroup/__tests__/Radio.test.tsx
@@ -0,0 +1,98 @@
+import { createRef } from 'react';
+
+import { screen, render } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
+import { describe, it, expect, vi } from 'vitest';
+
+import { Radio, RadioGroup, type RadioProps } from '../index';
+
+const RADIO__TEST_ID = 'RADIO';
+const RADIO__CHILDREN = 'CONTENT';
+const RADIO__VALUE = 'VALUE';
+
+const baseProps: RadioProps = {
+  'data-testid': RADIO__TEST_ID,
+  children: RADIO__CHILDREN,
+  value: RADIO__VALUE,
+};
+
+describe('Radio', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  const getRadio = () => screen.getByTestId(RADIO__TEST_ID);
+
+  it('should accept a ref', () => {
+    const ref = createRef<HTMLLabelElement>();
+
+    const { container } = render(
+      <RadioGroup aria-label="root">
+        <Radio {...baseProps} ref={ref} />
+      </RadioGroup>
+    );
+
+    const el = container.querySelector('label');
+    expect(ref.current).toBe(el);
+  });
+
+  it('should set a custom style', () => {
+    const style = { padding: 20 };
+
+    render(
+      <RadioGroup aria-label="root">
+        <Radio {...baseProps} style={style} />
+      </RadioGroup>
+    );
+
+    expect(getRadio()).toHaveStyle('padding: 20px');
+  });
+
+  it('should accept a custom class', () => {
+    render(
+      <RadioGroup aria-label="root">
+        <Radio {...baseProps} className="foo" />
+      </RadioGroup>
+    );
+
+    expect(getRadio()).toHaveClass('foo');
+  });
+
+  it('should display a content', () => {
+    render(
+      <RadioGroup aria-label="root">
+        <Radio {...baseProps} />
+      </RadioGroup>
+    );
+
+    expect(screen.getByText(RADIO__CHILDREN)).toBeInTheDocument();
+  });
+
+  it('should be disabled when disabled prop sets true', async () => {
+    const onChange = vi.fn();
+
+    const { container } = render(
+      <RadioGroup aria-label="root" onChange={onChange}>
+        <Radio {...baseProps} disabled />
+      </RadioGroup>
+    );
+
+    const el = container.querySelector('input');
+
+    expect(el).toBeDisabled();
+
+    await userEvent.click(screen.getByTestId(RADIO__TEST_ID));
+
+    expect(onChange).toBeCalledTimes(0);
+  });
+
+  it('should apply a correct size', () => {
+    render(
+      <RadioGroup aria-label="root" size="normal">
+        <Radio {...baseProps} size="big" />
+      </RadioGroup>
+    );
+
+    expect(getRadio()).toHaveAttribute('data-size', 'big');
+  });
+});
--- packages/components/src/components/RadioGroup/__tests__/RadioGroup.test.tsx
+++ packages/components/src/components/RadioGroup/__tests__/RadioGroup.test.tsx
@@ -1,29 +1,125 @@
 import { createRef } from 'react';
 
 import { screen, render } from '@testing-library/react';
-import { describe, it, expect } from 'vitest';
+import { userEvent } from '@testing-library/user-event';
+import { describe, it, expect, vi } from 'vitest';
 
-import { RadioGroup } from '../index';
+import { Radio, RadioGroup, type RadioGroupProps } from '../index';
+
+const RADIO__TEST_ID = 'RADIO';
+
+const baseProps: RadioGroupProps = {
+  'aria-label': 'root',
+};
+
+const renderComponent = (props: Omit<RadioGroupProps, 'children'>) => (
+  <RadioGroup {...baseProps} {...props}>
+    <Radio value="1">1</Radio>
+    <Radio value="2" data-testid={RADIO__TEST_ID}>
+      2
+    </Radio>
+    <Radio value="3">3</Radio>
+    <Radio value="4">4</Radio>
+  </RadioGroup>
+);
 
 describe('RadioGroup', () => {
-  const baseProps = {
-    'aria-label': 'root',
-  };
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
 
   const getRoot = () => screen.getByRole('radiogroup');
+  const getRadio = () => screen.getByTestId(RADIO__TEST_ID);
 
-  describe('business logic', () => {
-    it('should accept the ref', () => {
-      const ref = createRef<HTMLDivElement>();
-      const { container } = render(<RadioGroup {...baseProps} ref={ref} />);
-      const inputElement = container.querySelector('div');
-      expect(ref.current).toBe(inputElement);
-    });
+  it('should accept a ref', () => {
+    const ref = createRef<HTMLDivElement>();
+    const { container } = render(<RadioGroup {...baseProps} ref={ref} />);
+    const el = container.querySelector('div');
+    expect(ref.current).toBe(el);
+  });
 
-    it('should accept a custom class', () => {
-      render(<RadioGroup {...baseProps} className="foo" />);
+  it('should set a custom style', () => {
+    const style = { padding: 20 };
 
-      expect(getRoot()).toHaveClass('foo');
-    });
+    const { container } = render(
+      renderComponent({
+        style,
+      })
+    );
+
+    const firstElement = container.firstChild;
+
+    expect(firstElement).toHaveStyle('padding: 20px');
+  });
+
+  it('should accept a custom class', () => {
+    render(renderComponent({ className: 'foo' }));
+
+    expect(getRoot()).toHaveClass('foo');
+  });
+
+  it('should display a label', () => {
+    render(renderComponent({ label: 'bar' }));
+
+    expect(screen.getByText('bar')).toBeInTheDocument();
+  });
+
+  it('should display a description', () => {
+    render(renderComponent({ description: 'qux' }));
+
+    expect(screen.getByText('qux')).toBeInTheDocument();
+  });
+
+  it('should be invalid when error prop sets true', async () => {
+    const { rerender } = render(renderComponent({ error: true }));
+
+    expect(getRoot()).toHaveAttribute('aria-invalid', 'true');
+
+    rerender(renderComponent({ error: false }));
+
+    expect(getRoot()).not.toHaveAttribute('aria-invalid', 'true');
+  });
+
+  it('should be disabled when disabled prop sets true', async () => {
+    const onChange = vi.fn();
+
+    render(renderComponent({ disabled: true, onChange, value: '1' }));
+
+    expect(getRoot()).toHaveAttribute('aria-disabled', 'true');
+
+    await userEvent.click(screen.getByTestId(RADIO__TEST_ID));
+
+    expect(onChange).toBeCalledTimes(0);
+  });
+
+  it('should onChange get correct value', async () => {
+    const onChange = vi.fn();
+
+    render(
+      renderComponent({
+        onChange,
+        value: '1',
+      })
+    );
+
+    await userEvent.click(screen.getByTestId(RADIO__TEST_ID));
+
+    expect(onChange).toBeCalledWith('2');
+  });
+
+  it('should apply a correct size to a group', () => {
+    const { rerender } = render(renderComponent({ size: 'big' }));
+
+    expect(getRadio()).toHaveAttribute('data-size', 'big');
+
+    rerender(renderComponent({ size: 'normal' }));
+
+    expect(getRadio()).toHaveAttribute('data-size', 'normal');
+  });
+
+  it('should apply an orientation to a group', () => {
+    render(renderComponent({ orientation: 'horizontal' }));
+
+    expect(getRoot()).toHaveAttribute('aria-orientation', 'horizontal');
   });
 });
