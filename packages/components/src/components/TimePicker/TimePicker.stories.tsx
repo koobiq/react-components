828d9972d01ae5f52b9ad63f94a752839cc361fc
Kamil Emeleev <emeleev.kamil@gmail.com>
2025-07-23 12:40:43 +0300

    docs(TimePicker): add States story

--- packages/components/src/components/TimePicker/TimePicker.stories.tsx
+++ packages/components/src/components/TimePicker/TimePicker.stories.tsx
@@ -1,4 +1,7 @@
+import { Time } from '@internationalized/date';
 import type { Meta, StoryObj } from '@storybook/react';
+
+import { Grid } from '../Grid';
 
 import { TimePicker } from './index.js';
 
@@ -17,4 +20,35 @@ type Story = StoryObj<typeof meta>;
 
 export const Base: Story = {
   render: (args) => <TimePicker label="Time" {...args} />,
+};
+
+export const States: Story = {
+  render: () => (
+    <Grid gap="m" cols={{ xs: 1, s: 2, m: 2, l: 2 }}>
+      <TimePicker
+        label="Time"
+        errorMessage="isInvalid"
+        defaultValue={new Time(11, 45)}
+        isInvalid
+      />
+      <TimePicker
+        label="Time"
+        caption="isDisabled"
+        defaultValue={new Time(11, 45)}
+        isDisabled
+      />
+      <TimePicker
+        label="Time"
+        caption="isRequired"
+        defaultValue={new Time(11, 45)}
+        isRequired
+      />
+      <TimePicker
+        label="Time"
+        caption="isReadOnly"
+        defaultValue={new Time(11, 45)}
+        isReadOnly
+      />
+    </Grid>
+  ),
 };
