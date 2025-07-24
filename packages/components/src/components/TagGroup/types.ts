56c080f5d7eed729b5204840f683d50066411b9c
Kamil Emeleev <emeleev.kamil@gmail.com>
2025-07-24 17:01:36 +0300

    refactor(TagGroup): extend `AriaTagGroupProps` type

--- packages/components/src/components/TagGroup/types.ts
+++ packages/components/src/components/TagGroup/types.ts
@@ -6,18 +6,8 @@ import type {
   Ref,
 } from 'react';
 
+import type { ExtendableProps } from '@koobiq/react-core';
 import type { AriaTagGroupProps } from '@koobiq/react-primitives';
-
-export type TagGroupPropChildren<T extends object> =
-  AriaTagGroupProps<T>['children'];
-
-export type TagGroupPropItems<T extends object> = AriaTagGroupProps<T>['items'];
-
-export type TagGroupPropOnRemove<T extends object> =
-  AriaTagGroupProps<T>['onRemove'];
-
-export type TagGroupPropDisabledKeys<T extends object> =
-  AriaTagGroupProps<T>['disabledKeys'];
 
 export const tagGroupPropVariant = [
   'theme-fade',
@@ -28,33 +18,28 @@ export const tagGroupPropVariant = [
 
 export type TagGroupPropVariant = (typeof tagGroupPropVariant)[number];
 
-export type TagGroupProps<T extends object> = {
-  /** The contents of the collection. */
-  children?: TagGroupPropChildren<T>;
-  /** Item objects in the collection. */
-  items?: TagGroupPropItems<T>;
-  /** Handler that is called when a user deletes a tag.  */
-  onRemove?: TagGroupPropOnRemove<T>;
-  /**
-   * The variant to use.
-   * @default 'theme-fade'
-   */
-  variant?: TagGroupPropVariant;
-  /** The item keys that are disabled. These items cannot be selected, focused, or otherwise interacted with. */
-  disabledKeys?: TagGroupPropDisabledKeys<T>;
-  /** Ref to the HTML ul-element. */
-  ref?: Ref<HTMLElement>;
-  /** Additional CSS-classes. */
-  className?: string;
-  /** Unique identifier for testing purposes. */
-  'data-testid'?: string | number;
-  /** Inline styles. */
-  style?: CSSProperties;
-  /** The props used for each slot inside. */
-  slotProps?: {
-    root?: ComponentPropsWithRef<'div'>;
-  };
-};
+export type TagGroupProps<T extends object> = ExtendableProps<
+  {
+    /**
+     * The variant to use.
+     * @default 'theme-fade'
+     */
+    variant?: TagGroupPropVariant;
+    /** Ref to the HTML ul-element. */
+    ref?: Ref<HTMLElement>;
+    /** Additional CSS-classes. */
+    className?: string;
+    /** Unique identifier for testing purposes. */
+    'data-testid'?: string | number;
+    /** Inline styles. */
+    style?: CSSProperties;
+    /** The props used for each slot inside. */
+    slotProps?: {
+      root?: ComponentPropsWithRef<'div'>;
+    };
+  },
+  AriaTagGroupProps<T>
+>;
 
 export type TagGroupComponent = <T extends object>(
   props: TagGroupProps<T>
