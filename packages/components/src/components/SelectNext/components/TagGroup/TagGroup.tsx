f81278941dad80cd83ef7e07e608114c38a1a363
Kamil Emeleev <emeleev.kamil@gmail.com>
2025-08-11 18:42:59 +0300

    feat(SelectNext): add `limitTags` prop

--- packages/components/src/components/SelectNext/components/TagGroup/TagGroup.tsx
+++ packages/components/src/components/SelectNext/components/TagGroup/TagGroup.tsx
@@ -4,6 +4,7 @@ import { useLocalizedStringFormatter } from '@koobiq/react-core';
 import type { MultiSelectState } from '@koobiq/react-primitives';
 
 import intlMessages from '../../intl.json';
+import type { SelectPropLimitTags } from '../../types';
 import { Tag } from '../Tag';
 
 import s from './TagGroup.module.css';
@@ -15,10 +16,19 @@ export type TagGroupProps<T> = {
     isDisabled?: boolean;
     isRequired?: boolean;
   };
+  limitTags?: SelectPropLimitTags;
 };
 
-export const TagGroup: FC<TagGroupProps<unknown>> = ({ state, states }) => {
+export const TagGroup: FC<TagGroupProps<unknown>> = ({
+  state,
+  states,
+  limitTags,
+}) => {
   const { isDisabled, isInvalid } = states;
+
+  const hasResponsiveTags = limitTags === 'responsive';
+
+  // TODO: responsive tag logic should be somewhere here
 
   const stringFormatter = useLocalizedStringFormatter(intlMessages);
 
@@ -26,6 +36,7 @@ export const TagGroup: FC<TagGroupProps<unknown>> = ({ state, states }) => {
     <div
       aria-hidden
       className={s.base}
+      data-limit-tags={limitTags}
       aria-label={stringFormatter.format('selected items')}
     >
       {state.selectedItems?.map((item) => (
@@ -43,6 +54,7 @@ export const TagGroup: FC<TagGroupProps<unknown>> = ({ state, states }) => {
           {item.textValue}
         </Tag>
       ))}
+      {hasResponsiveTags && <div className={s.more}>n more</div>}
     </div>
   );
 };
