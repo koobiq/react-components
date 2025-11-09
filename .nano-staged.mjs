697de03819d649e1068868c1e269f2bde7303ab6
Kamil Emeleev <emeleev.kamil@gmail.com>
2025-11-09 18:32:40 +0300

    chore(stylelint): improve prettier integration and prevent double formatting

--- .nano-staged.mjs
+++ .nano-staged.mjs
@@ -1,8 +1,8 @@
 export default {
   '**/*.{js,jsx,ts,tsx}': ['eslint --fix', 'vitest related --run'],
-  '**/*.{js,jsx,ts,tsx,json,css,md,mdx,svg,html,yml,yaml}': [
-    'prettier --write',
-  ],
   '**/*.{css}': ['stylelint --fix'],
   '**/*': () => 'pnpm type-check',
+  '**/*.{json,md,mdx,svg,html,yml,yaml}': [
+    'prettier --write',
+  ],
 };
