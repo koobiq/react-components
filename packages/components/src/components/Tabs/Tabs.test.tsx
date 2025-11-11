1f7d08c0faa68d4bfc4ac137e809b6947efa9dba
Kamil Emeleev <emeleev.kamil@gmail.com>
2025-11-11 15:10:32 +0300

    test(Tabs): add scrolling tests

--- packages/components/src/components/Tabs/Tabs.test.tsx
+++ packages/components/src/components/Tabs/Tabs.test.tsx
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
 import { describe, expect, it, vi } from 'vitest';
 
 import { Tabs, Tab, type TabsProps } from './index';
+import s from './Tabs.module.css';
 
 const TAB__TEST_ID = 'TAB';
 
@@ -78,5 +79,152 @@ describe('Tabs', () => {
     await userEvent.click(screen.getByTestId(TAB__TEST_ID));
 
     expect(onSelectionChange).toBeCalledWith('2');
+  });
+
+  describe('scrolling', () => {
+    const ariaLabelPrevBtn = 'Previous tabs';
+    const ariaLabelNextBtn = 'Next tabs';
+
+    const findScrollButton = (container: HTMLElement, labelText: string) =>
+      container.querySelector(`button[aria-label="${labelText}"]`);
+
+    const hasPrevScrollButton = (container: HTMLElement) =>
+      !!findScrollButton(container, ariaLabelPrevBtn);
+
+    const hasNextScrollButton = (container: HTMLElement) =>
+      !!findScrollButton(container, ariaLabelNextBtn);
+
+    // Mock scrollTo method (jsdom doesn't support scrolling)
+    Object.defineProperty(HTMLElement.prototype, 'scrollTo', {
+      value(options?: ScrollToOptions | number, y?: number) {
+        if (typeof options === 'object' && options !== null) {
+          if (typeof options.left === 'number') {
+            this.scrollLeft = options.left;
+          }
+
+          if (typeof options.top === 'number') {
+            this.scrollTop = options.top;
+          }
+        } else if (typeof options === 'number') {
+          this.scrollLeft = options;
+          if (typeof y === 'number') this.scrollTop = y;
+        }
+      },
+      writable: true,
+    });
+
+    it('should render the next button when tab-list > scroll-box', () => {
+      const { container, rerender } = render(renderComponent({}));
+
+      const tabsList = container.querySelector(`.${s.tabList}`);
+      const scrollBox = container.querySelector(`.${s.scrollBox}`);
+
+      Object.defineProperty(tabsList, 'clientWidth', { value: 300 });
+      Object.defineProperty(scrollBox, 'clientWidth', { value: 200 });
+
+      rerender(renderComponent({}));
+
+      expect(hasNextScrollButton(container)).toBe(true);
+    });
+
+    it('should NOT render the next button when tab-list <= scroll-box', () => {
+      const { container, rerender } = render(renderComponent({}));
+
+      const tabsList = container.querySelector(`.${s.tabList}`);
+      const scrollBox = container.querySelector(`.${s.scrollBox}`);
+
+      Object.defineProperty(tabsList, 'clientWidth', {
+        value: 200,
+        configurable: true,
+      });
+
+      Object.defineProperty(scrollBox, 'clientWidth', {
+        value: 200,
+        configurable: true,
+      });
+
+      rerender(renderComponent({}));
+      expect(hasNextScrollButton(container)).toBe(false);
+
+      Object.defineProperty(tabsList, 'clientWidth', { value: 199 });
+      Object.defineProperty(scrollBox, 'clientWidth', { value: 200 });
+
+      rerender(renderComponent({}));
+      expect(hasNextScrollButton(container)).toBe(false);
+    });
+
+    it('should render next/prev buttons when content overflows and already scrolled', () => {
+      const { container, rerender } = render(renderComponent({}));
+
+      const tabsList = container.querySelector(`.${s.tabList}`);
+      const scrollBox = container.querySelector(`.${s.scrollBox}`);
+
+      Object.defineProperty(tabsList, 'clientWidth', { value: 300 });
+      Object.defineProperty(scrollBox, 'clientWidth', { value: 200 });
+      Object.defineProperty(scrollBox, 'scrollLeft', { value: 10 });
+
+      rerender(renderComponent({}));
+
+      expect(hasPrevScrollButton(container)).toBe(true);
+      expect(hasNextScrollButton(container)).toBe(true);
+    });
+
+    describe('scroll buttons behavior', () => {
+      vi.spyOn(window, 'requestAnimationFrame').mockImplementation((cb) => {
+        cb(0);
+
+        return 0;
+      });
+
+      vi.spyOn(global.Math, 'min').mockReturnValue(1);
+
+      it('should scroll to right when clicking the next button', () => {
+        const { container, rerender } = render(renderComponent({}));
+
+        const tabsList = container.querySelector(`.${s.tabList}`);
+        const scrollBox = container.querySelector(`.${s.scrollBox}`);
+
+        Object.defineProperty(tabsList, 'clientWidth', { value: 300 });
+        Object.defineProperty(scrollBox, 'clientWidth', { value: 200 });
+
+        Object.defineProperty(scrollBox, 'scrollLeft', {
+          value: 0,
+          configurable: true,
+          writable: true,
+        });
+
+        rerender(renderComponent({}));
+
+        const nextScrollButton = findScrollButton(container, ariaLabelNextBtn);
+
+        if (nextScrollButton) fireEvent.click(nextScrollButton);
+
+        expect(Number(scrollBox?.scrollLeft) > 0).toBeTruthy();
+      });
+    });
+
+    it('should scroll to left when clicking the prev button', () => {
+      const { container, rerender } = render(renderComponent({}));
+
+      const tabsList = container.querySelector(`.${s.tabList}`);
+      const scrollBox = container.querySelector(`.${s.scrollBox}`);
+
+      Object.defineProperty(tabsList, 'clientWidth', { value: 300 });
+      Object.defineProperty(scrollBox, 'clientWidth', { value: 200 });
+
+      Object.defineProperty(scrollBox, 'scrollLeft', {
+        value: 100,
+        configurable: true,
+        writable: true,
+      });
+
+      rerender(renderComponent({}));
+
+      const prevScrollButton = findScrollButton(container, ariaLabelPrevBtn);
+
+      if (prevScrollButton) fireEvent.click(prevScrollButton);
+
+      expect(Number(scrollBox?.scrollLeft) < 100).toBeTruthy();
+    });
   });
 });
