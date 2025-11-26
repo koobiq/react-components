4680620bc4707d5c9f74f1d22a0163804e9d4d10
Kamil Emeleev <emeleev.kamil@gmail.com>
2025-11-26 16:56:10 +0300

    docs(CheckboxGroup): add 'Caption' and 'Orientation' stories

--- packages/components/src/components/CheckboxGroup/CheckboxGroup.stories.tsx
+++ packages/components/src/components/CheckboxGroup/CheckboxGroup.stories.tsx
@@ -1,10 +1,13 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
+import { Checkbox } from '../Checkbox';
+
 import { CheckboxGroup } from './index';
 
 const meta = {
   title: 'Components/CheckboxGroup',
   component: CheckboxGroup,
+  subcomponents: { Checkbox },
   parameters: {
     layout: 'centered',
   },
@@ -16,5 +19,45 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Base: Story = {
-  render: (args) => <CheckboxGroup {...args}>Label</CheckboxGroup>,
+  render: (args) => (
+    <CheckboxGroup label="What are you interested in?" {...args}>
+      <Checkbox value="frontend">Frontend</Checkbox>
+      <Checkbox value="backend">Backend</Checkbox>
+      <Checkbox value="devops">DevOps</Checkbox>
+      <Checkbox value="design">Design</Checkbox>
+      <Checkbox value="product">Product</Checkbox>
+    </CheckboxGroup>
+  ),
+};
+
+export const Caption: Story = {
+  render: (args) => (
+    <CheckboxGroup
+      label="What are you interested in?"
+      caption="Select all options that apply."
+      {...args}
+    >
+      <Checkbox value="frontend">Frontend</Checkbox>
+      <Checkbox value="backend">Backend</Checkbox>
+      <Checkbox value="devops">DevOps</Checkbox>
+      <Checkbox value="design">Design</Checkbox>
+      <Checkbox value="product">Product</Checkbox>
+    </CheckboxGroup>
+  ),
+};
+
+export const Orientation: Story = {
+  render: (args) => (
+    <CheckboxGroup
+      label="What are you interested in?"
+      orientation="horizontal"
+      {...args}
+    >
+      <Checkbox value="frontend">Frontend</Checkbox>
+      <Checkbox value="backend">Backend</Checkbox>
+      <Checkbox value="devops">DevOps</Checkbox>
+      <Checkbox value="design">Design</Checkbox>
+      <Checkbox value="product">Product</Checkbox>
+    </CheckboxGroup>
+  ),
 };
