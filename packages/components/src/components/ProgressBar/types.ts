49b7f3eab102192cac1efd8b04a36cb86bf945d1
Kamil Emeleev <emeleev.kamil@gmail.com>
2025-11-27 07:29:17 +0300

    feat(ProgressBar): replace deprecated `variant` prop with `isIndeterminate`

--- packages/components/src/components/ProgressBar/types.ts
+++ packages/components/src/components/ProgressBar/types.ts
@@ -10,6 +10,16 @@ export const progressBarPropVariant = ['indeterminate', 'determinate'] as const;
 
 export type ProgressBarPropVariant = (typeof progressBarPropVariant)[number];
 
+type ProgressBarDeprecatedProps = {
+  /**
+   * The variant to use. Use indeterminate or query when there is no progress value.
+   * @default 'determinate'
+   * @deprecated
+   * The "variant" prop is deprecated. Use "isIndeterminate" prop to replace it.
+   */
+  variant?: ProgressBarPropVariant;
+};
+
 export type ProgressBarBaseProps = ExtendableComponentPropsWithRef<
   {
     children?: never;
@@ -17,11 +27,6 @@ export type ProgressBarBaseProps = ExtendableComponentPropsWithRef<
     className?: string;
     /** Inline styles. */
     style?: CSSProperties;
-    /**
-     * The variant to use. Use indeterminate or query when there is no progress value.
-     * @default 'determinate'
-     */
-    variant?: ProgressBarPropVariant;
     /** The props used for each slot inside. */
     slotProps?: {
       fill?: ComponentPropsWithRef<'span'>;
@@ -31,9 +36,9 @@ export type ProgressBarBaseProps = ExtendableComponentPropsWithRef<
 >;
 
 export type ProgressBarProps = ExtendableProps<
-  ProgressBarBaseProps,
+  ProgressBarBaseProps & ProgressBarDeprecatedProps,
   Omit<
     ProgressBarBasePrimitiveProps,
-    'children' | 'isIndeterminate' | 'label' | 'formatOptions' | 'valueLabel'
+    'children' | 'label' | 'formatOptions' | 'valueLabel'
   >
 >;
