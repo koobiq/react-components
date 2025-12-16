46948f60da280e9e67e730ed2e9d8ca04ad6e856
Kamil Emeleev <emeleev.kamil@gmail.com>
2025-12-16 11:49:10 +0300

    refactor(core): sync typing with React 19

--- packages/core/src/hooks/useDOMRef/useDOMRef.ts
+++ packages/core/src/hooks/useDOMRef/useDOMRef.ts
@@ -6,7 +6,7 @@ import { useImperativeHandle, useRef } from 'react';
 export function useDOMRef<T extends HTMLElement = HTMLElement>(
   ref?: RefObject<T | null> | Ref<T | null>
 ) {
-  const domRef = useRef<T>(null);
+  const domRef = useRef<T>(null!);
 
   useImperativeHandle(ref, () => domRef.current);
 
--- packages/core/src/hooks/useKeyedRefs/useKeyedRefs.ts
+++ packages/core/src/hooks/useKeyedRefs/useKeyedRefs.ts
@@ -1,18 +1,19 @@
 'use client';
 
-import { createRef, type RefObject, type Key, useRef } from 'react';
+import { createRef, useCallback, useRef } from 'react';
+import type { Key, RefObject } from 'react';
 
-export function useKeyedRefs<T>() {
-  const mapRef = useRef(new Map<Key, RefObject<T>>());
+export function useKeyedRefs<T extends HTMLElement = HTMLElement>() {
+  const mapRef = useRef<Map<Key, RefObject<T | null>>>(new Map());
 
-  return (key: Key): RefObject<T> => {
+  return useCallback((key: Key): RefObject<T | null> => {
     let ref = mapRef.current.get(key);
 
     if (!ref) {
-      ref = createRef<T>();
+      ref = createRef<T | null>();
       mapRef.current.set(key, ref);
     }
 
     return ref;
-  };
+  }, []);
 }
