ff5576fb253fcd6c774bb877c2eeec73819424ae
Kamil Emeleev <emeleev.kamil@gmail.com>
2025-12-16 14:55:30 +0300

    chore(templates): update Vite app template

--- templates/vite/template/src/App.tsx
+++ templates/vite/template/src/App.tsx
@@ -7,7 +7,7 @@ import {
   Typography,
 } from '@koobiq/react-components';
 
-import { IconChevronCircleRight16 } from '@koobiq/react-icons';
+import { IconCircleChevronRight16 } from '@koobiq/react-icons';
 
 import logo from './assets/koobiq.svg';
 
@@ -43,7 +43,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
           href="https://react.koobiq.io/"
-          startIcon={<IconChevronCircleRight16 />}
+          startIcon={<IconCircleChevronRight16 />}
         >
           Explore more
         </Button>
