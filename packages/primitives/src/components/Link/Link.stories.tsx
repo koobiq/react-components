0b055a02d4f9e819be4689924712d61efb030b21
Kamil Emeleev <emeleev.kamil@gmail.com>
2025-12-29 22:52:37 +0300

    docs(primitives): improve the documentation

--- packages/primitives/src/components/Link/Link.stories.tsx
+++ packages/primitives/src/components/Link/Link.stories.tsx
@@ -23,51 +23,34 @@ export const Base: Story = {
   render: (args: LinkBaseProps) => (
     <Link
       {...args}
-      style={({ isHovered, isPressed }) => {
+      style={({ isHovered, isPressed, isFocusVisible }) => {
         const commonStyle: CSSProperties = {
           fontSize: 16,
-          color: 'light-dark(#000, #fff)',
-          lineHeight: 24,
+          color: '#06f',
           outline: 'none',
+          outlineWidth: 2,
+          outlineOffset: 2,
+          lineHeight: '24px',
           position: 'relative',
+          outlineStyle: 'solid',
           textDecoration: 'none',
+          outlineColor: 'transparent',
           fontFamily: 'Inter, Arial, sans-serif',
-          transition: 'opacity 120ms ease-in-out',
+          transition:
+            'opacity 120ms ease-in-out, outline-color 120ms ease-in-out',
         };
 
         return {
           ...commonStyle,
           ...(isHovered && { opacity: 0.9 }),
           ...(isPressed && { opacity: 0.8 }),
+          ...(isFocusVisible && {
+            outlineColor: '#06f',
+          }),
         };
       }}
     >
-      {({ isHovered, isFocusVisible }) => (
-        <>
-          {
-            <div
-              style={
-                {
-                  transform: 'translateY(0)',
-                  ...((isHovered || isFocusVisible) && {
-                    transform: 'translateY(2px)',
-                  }),
-                  height: 2,
-                  bottom: 0,
-                  content: '',
-                  width: '100%',
-                  display: 'block',
-                  position: 'absolute',
-                  transition: 'transform 120ms ease-in-out',
-                  background:
-                    'linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)',
-                } as CSSProperties
-              }
-            />
-          }
-          Anchor
-        </>
-      )}
+      Anchor
     </Link>
   ),
 };
