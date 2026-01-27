bf9cd338b9d6d0885a22d11deb2c4d5d6c7e89c5
Kamil Emeleev <emeleev.kamil@gmail.com>
2026-01-27 16:06:12 +0300

    docs(ContentPanel): extend the documentation

--- packages/components/src/components/ContenPanel/ContentPanel.stories.tsx
+++ packages/components/src/components/ContenPanel/ContentPanel.stories.tsx
@@ -1,8 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { useBoolean } from '@koobiq/react-core';
+import {
+  IconArrowsCollapseDiagonal16,
+  IconArrowsExpandDiagonal16,
+  IconXmark16,
+} from '@koobiq/react-icons';
 import type { Meta, StoryObj } from '@storybook/react';
 
+import { AnimatedIcon } from '../AnimatedIcon';
 import { Autocomplete } from '../Autocomplete';
 import { Button } from '../Button';
 import { FlexBox } from '../FlexBox';
@@ -16,7 +22,12 @@ import { ContentPanel, ContentPanelContainer } from './index';
 const meta = {
   title: 'Components/ContentPanel',
   component: ContentPanel,
-  subcomponents: { ContentPanelContainer },
+  subcomponents: {
+    ContentPanelContainer,
+    'ContentPanel.Header': ContentPanel.Header,
+    'ContentPanel.Body': ContentPanel.Body,
+    'ContentPanel.Footer': ContentPanel.Footer,
+  },
   parameters: {
     layout: 'centered',
   },
@@ -27,81 +38,145 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Base: Story = {
-  render: (args) => (
-    <ContentPanelContainer defaultOpen style={{ blockSize: 300 }} {...args}>
-      <FlexBox direction="column" gap="m">
-        <Typography>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
-          commodi cupiditate facere fugiat in ipsa maiores necessitatibus, odit
-          quo quod rem repellat, sint, tenetur! Alias debitis dicta doloremque
-          enim fuga fugiat maiores qui rerum sint voluptatibus. A accusantium ad
-          assumenda cupiditate dolores eos excepturi fuga harum iusto molestias
-          nam pariatur perferendis quam quisquam quo quod, quos ut velit. Ad
-          architecto at consequatur consequuntur deleniti esse excepturi illo,
-          incidunt, maxime modi, nemo nobis quia sit. Consequatur earum, ex
-          pariatur sit temporibus veritatis. Eum explicabo laboriosam quae!
-          Alias aperiam at deleniti deserunt, dignissimos in, iste maxime
-          necessitatibus nobis quidem reiciendis, vel veritatis!
-        </Typography>
-        <Button slot="trigger">See details</Button>
-      </FlexBox>
-      <ContentPanel isResizable>
-        <ContentPanel.Header>ContentPanel</ContentPanel.Header>
-        <ContentPanel.Body>
-          <Select
-            label="Attack type"
-            style={{ inlineSize: 200 }}
-            placeholder="Select an option"
-          >
-            <Select.Item key="bruteforce">Bruteforce</Select.Item>
-            <Select.Item key="complex-attack">Complex Attack</Select.Item>
-            <Select.Item key="ddos">DDoS</Select.Item>
-            <Select.Item key="dos">DoS</Select.Item>
-            <Select.Item key="hips-alert">HIPS Alert</Select.Item>
-            <Select.Item key="ids-ips-alert">IDS/IPS Alert</Select.Item>
-            <Select.Item key="identity-theft">Identity Theft</Select.Item>
-            <Select.Item key="miscellaneous">Miscellaneous</Select.Item>
-            <Select.Item key="network-attack">Network Attack</Select.Item>
-            <Select.Item key="post-compromise">Post Compromise</Select.Item>
-            <Select.Item key="potential-attack">Potential Attack</Select.Item>
-          </Select>
-          <Autocomplete label="Protocol" placeholder="Search a protocol">
-            <Autocomplete.Item key="tls">TLS</Autocomplete.Item>
-            <Autocomplete.Item key="ssh">SSH</Autocomplete.Item>
-            <Autocomplete.Item key="pgp">PGP</Autocomplete.Item>
-            <Autocomplete.Item key="ipsec">IPSec</Autocomplete.Item>
-            <Autocomplete.Item key="kerberos">Kerberos</Autocomplete.Item>
-          </Autocomplete>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A corporis
-            debitis doloremque fugiat harum laudantium odit perferendis placeat
-            qui, repudiandae. Doloribus excepturi ipsum maxime! Animi error
-            ipsam quae ratione voluptatum. Accusamus aliquid autem commodi culpa
-            distinctio dolorem ducimus eaque, eveniet expedita facilis incidunt
-            labore laboriosam minus molestias non, officiis pariatur porro
-            possimus quas quos rem sunt suscipit tenetur ut vel velit vero.
-            Libero, perspiciatis, repellat? Earum eligendi et magni
-            necessitatibus non quos sed, similique soluta tempore? Accusantium,
-            alias animi ea molestiae nulla voluptates voluptatum. A ab ad
-            aperiam blanditiis, commodi consectetur dolores et eveniet harum id
-            ipsam necessitatibus nemo, nisi praesentium quibusdam quis
-            reprehenderit temporibus ullam ut veritatis. A ab accusamus,
-            adipisci aliquid animi aut consectetur cum cumque cupiditate dicta
-            doloremque ducimus eligendi eveniet ex fuga fugiat illum incidunt
-            ipsum iure iusto laborum maiores, molestias necessitatibus nulla
-            numquam odio officiis perferendis possimus quia quibusdam ratione
-            repudiandae sapiente unde ut vel voluptas voluptate. A accusamus
-            aperiam architecto atque commodi cum ducimus earum et excepturi
-            facere fuga fugiat, harum id ipsam itaque molestiae molestias, nemo
-            nulla, porro quaerat quas quasi quibusdam ratione repellendus rerum
-            saepe sed sint soluta tempora tenetur ullam vel veniam veritatis
-            vero voluptas voluptate voluptates! Alias necessitatibus odit
-            repudiandae.
-          </Typography>
-        </ContentPanel.Body>
-      </ContentPanel>
-    </ContentPanelContainer>
-  ),
+  render: function Render(args) {
+    const [isExpanded, setExpanded] = useBoolean(false);
+    const collapsedSize = 300;
+    const [width, onResize] = useState(collapsedSize);
+
+    useEffect(() => {
+      if (isExpanded) {
+        onResize(480);
+      } else {
+        onResize(collapsedSize);
+      }
+    }, [isExpanded]);
+
+    return (
+      <ContentPanelContainer
+        style={{
+          blockSize: 300,
+          inlineSize: 480,
+          borderRadius: 16,
+          backgroundColor: 'var(--kbq-background-bg-secondary)',
+        }}
+        {...args}
+      >
+        {({ toggle, isOpen, close }) => (
+          <>
+            <FlexBox
+              gap="m"
+              direction="column"
+              alignItems="center"
+              justifyContent="center"
+              style={{ inlineSize: '100%', blockSize: '100%' }}
+            >
+              <Button onPress={toggle}>
+                {isOpen ? 'Close panel' : 'Open panel'}
+              </Button>
+            </FlexBox>
+            <ContentPanel
+              width={width}
+              onResize={onResize}
+              isResizable
+              hideCloseButton
+            >
+              <ContentPanel.Header>
+                <FlexBox
+                  gap="xs"
+                  alignItems="center"
+                  style={{ inlineSize: '100%' }}
+                  justifyContent="space-between"
+                >
+                  <Typography variant="inherit" ellipsis>
+                    Web Security
+                  </Typography>
+                  <FlexBox>
+                    <Button
+                      aria-label={isExpanded ? 'Collapse' : 'Expand'}
+                      variant="contrast-transparent"
+                      onPress={setExpanded.toggle}
+                      startIcon={
+                        <AnimatedIcon
+                          icons={[
+                            <IconArrowsExpandDiagonal16 key="expand" />,
+                            <IconArrowsCollapseDiagonal16 key="сollapse" />,
+                          ]}
+                          activeIndex={+isExpanded}
+                          {...args}
+                        />
+                      }
+                      onlyIcon
+                    />
+
+                    <Button
+                      onPress={close}
+                      aria-label="Close"
+                      variant="contrast-transparent"
+                      startIcon={<IconXmark16 />}
+                      onlyIcon
+                    />
+                  </FlexBox>
+                </FlexBox>
+              </ContentPanel.Header>
+              <ContentPanel.Body>
+                <FlexBox gap="m" direction="column">
+                  <span>
+                    Web security is a crucial aspect of modern digital
+                    infrastructure, ensuring the protection of sensitive data,
+                    user privacy, and system integrity. As cyber threats
+                    continue to evolve, developers and organizations must adopt
+                    a proactive approach to securing web applications against
+                    attacks.
+                  </span>
+                  <span>
+                    One of the most common vulnerabilities is SQL injection,
+                    where attackers manipulate database queries to gain
+                    unauthorized access to sensitive information. Similarly,
+                    cross-site scripting (XSS) allows malicious scripts to run
+                    on a victim’s browser, leading to data theft or session
+                    hijacking. Another prevalent threat is cross-site request
+                    forgery (CSRF), in which users are tricked into executing
+                    unwanted actions on authenticated sites. Additionally,
+                    man-in-the-middle attacks intercept communication between
+                    users and servers, compromising the confidentiality of data.
+                    Distributed Denial-of-Service (DDoS) attacks can also
+                    cripple web services by overwhelming them with excessive
+                    traffic.
+                  </span>
+                  <span>
+                    To mitigate these risks, implementing strong security
+                    practices is essential. Using HTTPS ensures encrypted
+                    communication, protecting data from interception. Proper
+                    input validation and escaping mechanisms help prevent code
+                    injection attacks. Authentication and authorization
+                    mechanisms, including multi-factor authentication (MFA) and
+                    role-based access control (RBAC), add layers of security to
+                    user access. Secure API development, including
+                    authentication, rate limiting, and encryption, reduces
+                    vulnerabilities in web services. Keeping software,
+                    frameworks, and dependencies up to date minimizes the risk
+                    of exploiting known vulnerabilities. Continuous monitoring,
+                    logging, and security audits help detect and respond to
+                    threats before they cause significant damage.
+                  </span>
+                  <span>
+                    Web security is not a one-time implementation but an ongoing
+                    process that evolves alongside emerging threats. By
+                    following best practices and staying vigilant, businesses
+                    and developers can build resilient, secure applications that
+                    protect users and data in an increasingly connected world.
+                  </span>
+                </FlexBox>
+              </ContentPanel.Body>
+              <ContentPanel.Footer>
+                <Button variant="fade-contrast-filled">Show more</Button>
+              </ContentPanel.Footer>
+            </ContentPanel>
+          </>
+        )}
+      </ContentPanelContainer>
+    );
+  },
 };
 
 export const Triggers: Story = {
@@ -392,4 +467,90 @@ export const Standalone: Story = {
       </div>
     );
   },
+};
+
+export const Sandbox: Story = {
+  render: (args) => (
+    <ContentPanelContainer defaultOpen style={{ blockSize: 300 }} {...args}>
+      {({ toggle, isOpen }) => (
+        <>
+          <FlexBox direction="column" gap="m">
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
+              commodi cupiditate facere fugiat in ipsa maiores necessitatibus,
+              odit quo quod rem repellat, sint, tenetur! Alias debitis dicta
+              doloremque enim fuga fugiat maiores qui rerum sint voluptatibus. A
+              accusantium ad assumenda cupiditate dolores eos excepturi fuga
+              harum iusto molestias nam pariatur perferendis quam quisquam quo
+              quod, quos ut velit. Ad architecto at consequatur consequuntur
+              deleniti esse excepturi illo, incidunt, maxime modi, nemo nobis
+              quia sit. Consequatur earum, ex pariatur sit temporibus veritatis.
+              Eum explicabo laboriosam quae! Alias aperiam at deleniti deserunt,
+              dignissimos in, iste maxime necessitatibus nobis quidem
+              reiciendis, vel veritatis!
+            </Typography>
+            <Button onPress={toggle}>{isOpen ? 'Close' : 'Open'}</Button>
+          </FlexBox>
+          <ContentPanel isResizable>
+            <ContentPanel.Header>ContentPanel</ContentPanel.Header>
+            <ContentPanel.Body>
+              <Select
+                label="Attack type"
+                style={{ inlineSize: 200 }}
+                placeholder="Select an option"
+              >
+                <Select.Item key="bruteforce">Bruteforce</Select.Item>
+                <Select.Item key="complex-attack">Complex Attack</Select.Item>
+                <Select.Item key="ddos">DDoS</Select.Item>
+                <Select.Item key="dos">DoS</Select.Item>
+                <Select.Item key="hips-alert">HIPS Alert</Select.Item>
+                <Select.Item key="ids-ips-alert">IDS/IPS Alert</Select.Item>
+                <Select.Item key="identity-theft">Identity Theft</Select.Item>
+                <Select.Item key="miscellaneous">Miscellaneous</Select.Item>
+                <Select.Item key="network-attack">Network Attack</Select.Item>
+                <Select.Item key="post-compromise">Post Compromise</Select.Item>
+                <Select.Item key="potential-attack">
+                  Potential Attack
+                </Select.Item>
+              </Select>
+              <Autocomplete label="Protocol" placeholder="Search a protocol">
+                <Autocomplete.Item key="tls">TLS</Autocomplete.Item>
+                <Autocomplete.Item key="ssh">SSH</Autocomplete.Item>
+                <Autocomplete.Item key="pgp">PGP</Autocomplete.Item>
+                <Autocomplete.Item key="ipsec">IPSec</Autocomplete.Item>
+                <Autocomplete.Item key="kerberos">Kerberos</Autocomplete.Item>
+              </Autocomplete>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
+                corporis debitis doloremque fugiat harum laudantium odit
+                perferendis placeat qui, repudiandae. Doloribus excepturi ipsum
+                maxime! Animi error ipsam quae ratione voluptatum. Accusamus
+                aliquid autem commodi culpa distinctio dolorem ducimus eaque,
+                eveniet expedita facilis incidunt labore laboriosam minus
+                molestias non, officiis pariatur porro possimus quas quos rem
+                sunt suscipit tenetur ut vel velit vero. Libero, perspiciatis,
+                repellat? Earum eligendi et magni necessitatibus non quos sed,
+                similique soluta tempore? Accusantium, alias animi ea molestiae
+                nulla voluptates voluptatum. A ab ad aperiam blanditiis, commodi
+                consectetur dolores et eveniet harum id ipsam necessitatibus
+                nemo, nisi praesentium quibusdam quis reprehenderit temporibus
+                ullam ut veritatis. A ab accusamus, adipisci aliquid animi aut
+                consectetur cum cumque cupiditate dicta doloremque ducimus
+                eligendi eveniet ex fuga fugiat illum incidunt ipsum iure iusto
+                laborum maiores, molestias necessitatibus nulla numquam odio
+                officiis perferendis possimus quia quibusdam ratione repudiandae
+                sapiente unde ut vel voluptas voluptate. A accusamus aperiam
+                architecto atque commodi cum ducimus earum et excepturi facere
+                fuga fugiat, harum id ipsam itaque molestiae molestias, nemo
+                nulla, porro quaerat quas quasi quibusdam ratione repellendus
+                rerum saepe sed sint soluta tempora tenetur ullam vel veniam
+                veritatis vero voluptas voluptate voluptates! Alias
+                necessitatibus odit repudiandae.
+              </Typography>
+            </ContentPanel.Body>
+          </ContentPanel>
+        </>
+      )}
+    </ContentPanelContainer>
+  ),
 };
