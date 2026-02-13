e466bbe638cb1d522a98a9072a25a654f2d175a2
Kamil Emeleev <emeleev.kamil@gmail.com>
2026-02-13 17:14:12 +0300

    fix(SelectNext): resolve type issues in utils

--- packages/components/src/components/SelectNext/utils.ts
+++ packages/components/src/components/SelectNext/utils.ts
@@ -1,25 +1,33 @@
 import { useCachedChildren } from '@koobiq/react-primitives';
+import type { Collection, Node } from '@react-types/shared';
 
-export function useCollectionRender(collection: any, parent: any) {
+export function useCollectionRender<T>(
+  collection: Collection<Node<T>>,
+  parent: Node<T> | null
+) {
   return useCachedChildren({
     items: parent ? collection.getChildren!(parent.key) : collection,
     dependencies: [],
-    children(node: any) {
+    children(node) {
       return node.render!(node);
     },
   });
 }
 
-export function CollectionRoot({ collection }: { collection: any }) {
+export function CollectionRoot<T>({
+  collection,
+}: {
+  collection: Collection<Node<T>>;
+}) {
   return useCollectionRender(collection, null);
 }
 
-export function CollectionBranch({
+export function CollectionBranch<T>({
   collection,
   parent,
 }: {
-  collection: any;
-  parent: any;
+  collection: Collection<Node<T>>;
+  parent: Node<T>;
 }) {
   return useCollectionRender(collection, parent);
 }
