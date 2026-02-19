8b8b9db8c1ff550d2fece57e9d1c31c1cf2c1f05
Kamil Emeleev <emeleev.kamil@gmail.com>
2026-02-19 12:56:25 +0300

    docs(ActionsPanel): fix Standalone story (round 2)

--- packages/components/src/components/ActionsPanel/ActionsPanel.stories.tsx
+++ packages/components/src/components/ActionsPanel/ActionsPanel.stories.tsx
@@ -398,12 +398,23 @@ export const Standalone: Story = {
 
     return (
       <>
-        <UsersTable
+        <List
           selectionMode="multiple"
           selectedKeys={selectedKeys}
           onSelectionChange={setSelectedKeys}
-          users={users}
-        />
+          aria-label="List with actions panel"
+        >
+          <List.Item key="item-1">Item 1</List.Item>
+          <List.Item key="item-2">Item 2</List.Item>
+          <List.Item key="item-3">Item 3</List.Item>
+          <List.Item key="item-4">Item 4</List.Item>
+          <List.Item key="item-5">Item 5</List.Item>
+          <List.Item key="item-6">Item 6</List.Item>
+          <List.Item key="item-7">Item 7</List.Item>
+          <List.Item key="item-8">Item 8</List.Item>
+          <List.Item key="item-9">Item 9</List.Item>
+          <List.Item key="item-10">Item 10</List.Item>
+        </List>
         <ActionsPanel
           selectedItemCount={selectedKeys === 'all' ? 'all' : selectedKeys.size}
           onClearSelection={() => {
