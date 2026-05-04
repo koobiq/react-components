49c0a9f173dad8110ecca61c405bc0ccaf6c3a82
Kamil Emeleev <emeleev.kamil@gmail.com>
2026-05-04 17:28:02 +0300

    docs(storybook): fix colors in documentation (DS-5048)

--- .storybook/themes.ts
+++ .storybook/themes.ts
@@ -15,7 +15,7 @@ export const light = create({
   ...brand,
   base: 'light',
   textColor: 'hsla(221, 100%, 5.1%, 0.85)', // var(--kbq-foreground-contrast)
-  colorSecondary: 'hsla(217, 100%, 15.5%, 0.11)', // var(--kbq-states-background-transparent-active)
+  colorSecondary: 'hsla(221, 100%, 5.1%, 0.85)', // var(--kbq-foreground-contrast)
   barBg: 'hsla(0, 0%, 100%, 1)', // var(--kbq-background-bg)
   barHoverColor: 'hsla(221, 100%, 5.1%, 0.85)', // var(--kbq-button-transparent-contrast-fade-on-left-icon)
   barTextColor: 'hsla(221, 100%, 5.1%, 0.85)', // var(--kbq-button-transparent-contrast-fade-on-left-icon)
@@ -28,13 +28,14 @@ export const light = create({
   buttonBorder: 'hsla(223, 100%, 12.5%, 0.24)', // var(--kbq-form-field-default-border-color)
   buttonBg: 'hsla(217, 28.5%, 96.3%, 1)', // var(--kbq-background-bg-secondary)
   brandImage: '/images/logo-name-light.svg',
+  appHoverBg: 'hsla(217, 100%, 15%, 0.11)', // kbq-states-background-transparent-active
 });
 
 export const dark = create({
   ...brand,
   base: 'dark',
   textColor: 'hsla(212, 100%, 96.8%, 0.9)', // var(--kbq-foreground-contrast)
-  colorSecondary: 'hsla(224, 100%, 85.8%, 0.17)', // var(--kbq-states-background-transparent-active)
+  colorSecondary: 'hsla(212, 100%, 96.8%, 0.9)', // var(--kbq-foreground-contrast)
   barBg: 'hsla(225, 20.1%, 9.5%, 1)', // var(--kbq-background-bg)
   barHoverColor: 'hsla(216, 100%, 95.6%, 0.83)', // var(--kbq-button-transparent-contrast-fade-on-left-icon)
   barTextColor: 'hsla(216, 100%, 95.6%, 0.83)', // var(--kbq-button-transparent-contrast-fade-on-left-icon)
@@ -47,4 +48,5 @@ export const dark = create({
   buttonBorder: 'hsla(219, 100%, 84.7%, 0.29)', // var(--kbq-form-field-default-border-color)
   buttonBg: 'hsla(224, 17.7%, 14.3%, 1)', // var(--kbq-background-bg-secondary)
   brandImage: '/images/logo-name-dark.svg',
+  appHoverBg: 'hsla(238, 59%, 78%, 0.17)', // kbq-states-background-transparent-active
 });
