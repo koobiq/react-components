f60a1d0c8f45023c47e961bda402519206e6f17a
K@mil <emeleev.kamil@gmail.com>
2026-05-13 13:45:39 +0300

    refactor(Alert): reuse `IconItem` in Alert status icon (DS-5088) (#337)

--- packages/components/src/components/Alert/components/AlertIcon/AlertIcon.tsx
+++ packages/components/src/components/Alert/components/AlertIcon/AlertIcon.tsx
@@ -2,18 +2,23 @@ import { forwardRef } from 'react';
 
 import { clsx } from '@koobiq/react-core';
 
+import { IconItem } from '../../../IconItem';
+
 import s from './AlertIcon.module.css';
 import type { AlertIconProps } from './types';
-import { matchStatusToIcon } from './utils';
+import { matchStatusToIcon, matchStatusToIconItemColor } from './utils';
 
 export const AlertIcon = forwardRef<HTMLDivElement, AlertIconProps>(
-  ({ status = 'info', icon, isCompact }, ref) => (
-    <div
-      className={clsx(s.base, status && s[status], isCompact && s.compact)}
+  ({ status = 'info', icon, isCompact, className, ...other }, ref) => (
+    <IconItem
+      {...other}
+      color={matchStatusToIconItemColor[status]}
+      variant={isCompact ? 'fade' : 'solid'}
+      className={clsx(s.base, isCompact && s.compact, className)}
       ref={ref}
     >
       {icon || matchStatusToIcon[isCompact ? 'compact' : 'normal'][status]}
-    </div>
+    </IconItem>
   )
 );
 
