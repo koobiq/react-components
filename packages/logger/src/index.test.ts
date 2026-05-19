e21de6c0fe007df923b28a3e477f6f2eecd51401
Kamil Emeleev <emeleev.kamil@gmail.com>
2026-05-19 20:07:09 +0300

    chore(deps): bump `typescript` from 5.7.3 to 6.03

--- packages/logger/src/index.test.ts
+++ packages/logger/src/index.test.ts
@@ -3,51 +3,51 @@ import { describe, beforeEach, afterAll, it, expect, vi } from 'vitest';
 import { logger, once, setConfig } from './index.js';
 
 describe('logger', () => {
-  const initialConsole = { ...global.console };
+  const initialConsole = { ...globalThis.console };
   setConfig({ scope: undefined });
 
   beforeEach(() => {
-    global.console.trace = vi.fn();
-    global.console.debug = vi.fn();
-    global.console.log = vi.fn();
-    global.console.info = vi.fn();
-    global.console.warn = vi.fn();
-    global.console.error = vi.fn();
+    globalThis.console.trace = vi.fn();
+    globalThis.console.debug = vi.fn();
+    globalThis.console.log = vi.fn();
+    globalThis.console.info = vi.fn();
+    globalThis.console.warn = vi.fn();
+    globalThis.console.error = vi.fn();
   });
 
   afterAll(() => {
-    global.console = initialConsole;
+    globalThis.console = initialConsole;
   });
 
   describe('logger', () => {
     it('should output a console message with the debug type', () => {
       const message = 'debug message';
       logger.debug(message);
-      expect(global.console.debug).toHaveBeenCalledWith(message);
+      expect(globalThis.console.debug).toHaveBeenCalledWith(message);
     });
 
     it('should output a console message with the log type', () => {
       const message = 'log message';
       logger.log(message);
-      expect(global.console.log).toHaveBeenCalledWith(message);
+      expect(globalThis.console.log).toHaveBeenCalledWith(message);
     });
 
     it('should output a console message with the info type', () => {
       const message = 'information message';
       logger.info(message);
-      expect(global.console.info).toHaveBeenCalledWith(message);
+      expect(globalThis.console.info).toHaveBeenCalledWith(message);
     });
 
     it('should output a console message with the warn type', () => {
       const message = 'warning message';
       logger.warn(message);
-      expect(global.console.warn).toHaveBeenCalledWith(message);
+      expect(globalThis.console.warn).toHaveBeenCalledWith(message);
     });
 
     it('should output a console message with the error type', () => {
       const message = 'error message';
       logger.error(message);
-      expect(global.console.error).toHaveBeenCalledWith(message);
+      expect(globalThis.console.error).toHaveBeenCalledWith(message);
     });
   });
 
@@ -60,23 +60,23 @@ describe('logger', () => {
       once.error('first');
       once.error('first');
 
-      expect(global.console.error).toBeCalledTimes(1);
+      expect(globalThis.console.error).toBeCalledTimes(1);
 
       once.error('second');
 
-      expect(global.console.error).toBeCalledTimes(2);
+      expect(globalThis.console.error).toBeCalledTimes(2);
     });
 
     it('should clear the message array when calling method clear', () => {
       once.error('first');
       once.error('first');
 
-      expect(global.console.error).toBeCalledTimes(1);
+      expect(globalThis.console.error).toBeCalledTimes(1);
 
       once.clear();
 
       once.error('first');
-      expect(global.console.error).toBeCalledTimes(2);
+      expect(globalThis.console.error).toBeCalledTimes(2);
     });
   });
 });
